3d5ab82a4ba1b7f360b9886db259c149fc5d5bd5
bddevfarid <faridulislam@Mac-mini.localdomain>
2024-02-01 12:25:18 +0600

    Link issue fixed in isolate widget

--- bdthemes-prime-slider.php
+++ bdthemes-prime-slider.php
@@ -4,21 +4,21 @@
  * Plugin Name: Prime Slider
  * Plugin URI: https://primeslider.pro/
  * Description: Prime Slider is a packed of elementor widget that gives you some awesome header and slider combination for your website.
- * Version: 3.11.12
+ * Version: 3.11.13
  * Author: BdThemes
  * Author URI: https://bdthemes.com/
  * Text Domain: bdthemes-prime-slider
  * Domain Path: /languages
  * License: GPL3
  * Elementor requires at least: 3.0.0
- * Elementor tested up to: 3.18.3
+ * Elementor tested up to: 3.19.0
  *
  */
 
 // Some pre define value for easy use
 
 if ( ! defined( 'BDTPS_CORE_VER' ) ) {
-	define( 'BDTPS_CORE_VER', '3.11.12' );
+	define( 'BDTPS_CORE_VER', '3.11.13' );
 }
 if ( ! defined( 'BDTPS_CORE__FILE__' ) ) {
 	define( 'BDTPS_CORE__FILE__', __FILE__ );
