494e9ae114caedcac1f1afdf9d05d8be43935b33
Shahidul Islam <bdkoder@gmail.com>
2024-04-16 16:10:19 +0600

    Stability updated

--- bdthemes-prime-slider.php
+++ bdthemes-prime-slider.php
@@ -11,7 +11,7 @@
  * Domain Path: /languages
  * License: GPL3
  * Elementor requires at least: 3.0.0
- * Elementor tested up to: 3.20.3
+ * Elementor tested up to: 3.21.1
  */
 
 // Some pre define value for easy use
