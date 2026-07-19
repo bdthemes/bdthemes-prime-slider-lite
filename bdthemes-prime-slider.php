8dabec20e741e6b0c1986981813e36d63db23df0
Faridul Islam <73481902+bddevfarid@users.noreply.github.com>
2026-07-19 16:26:39 +0600

    update ready

--- bdthemes-prime-slider.php
+++ bdthemes-prime-slider.php
@@ -4,7 +4,7 @@
  * Plugin Name: Prime Slider
  * Plugin URI: https://primeslider.pro/
  * Description: Elementor addon pack for building responsive headers and sliders (hero, posts, WooCommerce, and more).
- * Version: 4.4.3
+ * Version: 4.4.4
  * Author: BdThemes
  * Author URI: https://bdthemes.com/
  * Text Domain: bdthemes-prime-slider
@@ -12,7 +12,7 @@
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Elementor requires at least: 4.0.0
- * Elementor tested up to: 4.1.4
+ * Elementor tested up to: 4.1.5
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Some pre define value for easy use
 
 if ( ! defined( 'BDTPS_CORE_VER' ) ) {
-	define( 'BDTPS_CORE_VER', '4.4.3' );
+	define( 'BDTPS_CORE_VER', '4.4.4' );
 }
 if ( ! defined( 'BDTPS_CORE__FILE__' ) ) {
 	define( 'BDTPS_CORE__FILE__', __FILE__ );
